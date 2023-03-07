62f1191ae73d3af33f6d132da47cd5797e5814dd
Dudu7352 <aeiou7352@gmail.com>
2023-03-07 09:09:16 +0100

    Jenkinsfile updated

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,27 @@
 use std::{iter, thread, time::Duration};
 use terminal_size::{terminal_size, Height, Width};
 
+const DURATION: Duration = Duration::from_millis(5);
+const PI: f64 = 3.1416;
+const WORD: &'static str = "Snake";
+
 fn main() {
-    const PI: f64 = 3.1416;
-    let mut step: f64 = 0.0;
-    let word = "Snake";
+    let mut cycle: f64 = 0.0;
     let size = terminal_size();
-    if let Some((Width(term_width), Height(_height))) = size {
-        let width = term_width as f64 - word.len() as f64;
-        loop {
-            let space_count = (width * (step.sin() + 1.0) / 2.0).abs() as usize;
-            let space = iter::repeat(" ").take(space_count).collect::<String>();
+    let mut space_iter = iter::repeat(" ");
+    let (Width(term_width), Height(_)) = size.expect("Oopsie doopsie something went wrongie!");
+    let width = term_width as f64 - WORD.len() as f64;
+    let step = PI / width as f64;
+    loop {
+        let space_count = (width * (-cycle.cos() + 1.0) / 2.0).abs() as usize;
+        let space = (&mut space_iter).take(space_count).collect::<String>();
 
-            println!("{}{}", space, word);
+        println!("{}{}", space, WORD);
 
-            step += PI / width as f64;
-            if step > PI * 2.0 {
-                step = step % PI * 2.0;
-            }
-            thread::sleep(Duration::from_millis(5));
+        cycle += step;
+        if cycle > PI * 2.0 {
+            cycle = cycle %  (PI * 2.0);
         }
-    } else {
-        println!("Unable to get terminal size");
+        thread::sleep(DURATION);
     }
 }
