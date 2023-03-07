79b9381b8bb7c70361eb931a5a67270913f365c0
lechos22 <63498252+lechos22@users.noreply.github.com>
2023-03-07 13:15:47 +0100

    created pattern_pregenerate()

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,32 @@
-use std::{iter, thread, time::Duration};
+use std::{iter::{repeat, Iterator}, thread, time::Duration};
 use terminal_size::{terminal_size, Height, Width};
 use std::f64::consts::PI;
 
 const DURATION: Duration = Duration::from_millis(5);
 const WORD: &'static str = "Snake";
 
-fn main() {
+fn n_snakes(n: usize, space_iter: &mut impl Iterator<Item = char>) -> String {
+    let space = space_iter.take(n).collect::<String>();
+    format!("{}{}", space, WORD)
+}
+
+fn pregenerate_pattern(/*todo: configuration struct*/) -> Vec<String> {
     let mut cycle: f64 = 0.0;
     let size = terminal_size();
-    let mut space_iter = iter::repeat(" ");
+    let mut space_iter = repeat(' ');
     let (Width(term_width), Height(_)) = size.expect("Oopsie doopsie something went wrongie!");
     let width = term_width as f64 - WORD.len() as f64;
     let step = PI / width as f64;
-    loop {
+    let mut pat = Vec::<String>::new();
+    while cycle < PI * 2.0 {
         let space_count = (width * (-cycle.cos() + 1.0) / 2.0).abs() as usize;
-        let space = (&mut space_iter).take(space_count).collect::<String>();
-
-        println!("{}{}", space, WORD);
-
+        pat.push(n_snakes(space_count, &mut space_iter));
         cycle += step;
-        if cycle > PI * 2.0 {
-            cycle = cycle %  (PI * 2.0);
-        }
-        thread::sleep(DURATION);
     }
+    return pat;
+}
+
+fn main() {
+    pregenerate_pattern().iter().for_each(|el| println!("{}", el));
+    thread::sleep(DURATION);
 }
